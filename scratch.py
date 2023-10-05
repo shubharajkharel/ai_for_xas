3e16c991b970eee56851860ee55e9646db14deff
shubharajkharel <shubha.raj.kharel@gmail.com>
2023-10-05 18:14:55 -0400

    xas data module

--- scratch.py
+++ scratch.py
@@ -1,7 +1,17 @@
 # %%
-
-import numpy as np
-import torch
-
+# %load_ext autoreload
+# %autoreload 2
 # %%
 
+import pytorch_lightning as pl
+import torch
+from src.dataset import XASDataModule
+from utils.src.optuna.dynamic_fc import PlDynamicFC
+
+
+model = PlDynamicFC(widths=[800, 400], output_size=200)
+trainer = pl.Trainer(max_epochs=100)
+data_module = XASDataModule(dtype=torch.float32, num_workers=0)
+trainer.fit(model, data_module)
+
+# %%
---
+++ tests/test_datamodule.py
@@ -0,0 +1,14 @@
+import unittest
+from src.dataset import XASDataModule
+import torch
+
+
+class TestXASDataModule(unittest.TestCase):
+    def test_load_dataset(self):
+        data_module = XASDataModule()
+        self.assertEqual(data_module.train_dataset[0][0].shape, torch.Size([64]))
+        self.assertEqual(data_module.train_dataset[0][1].shape, torch.Size([200]))
+
+
+if __name__ == "__main__":
+    unittest.main()
